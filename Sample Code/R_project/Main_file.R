3f0a9ef9422f0745a4fc0d076969881bd2cd6273
LJanys <56441930+LJanys@users.noreply.github.com>
2021-11-02 09:41:55 +0100

    Upload_PS1

--- Sample Code/R_project/Main_file.R
+++ Sample Code/R_project/Main_file.R
@@ -1,15 +1,15 @@
 ######################Main File#################################
 ###Write my main analysis##############
-
-
+####Source the functions###################
+source("~/Documents/GitHub/Computergest_Statistik/Sample Code/R_project/functions.R")
 ########Numerical/hardcoded values##############################
-a=1
-b=2
-
+c=1
+d=2
+e=10
 
 ###Call calculator############
 
-
+calculator(c,d,e)
 
 
 
